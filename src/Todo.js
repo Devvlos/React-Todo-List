b31a36e219e4f9b60acd2f97273997c00ab369db
Devvlos <devcantcode@gmail.com>
2024-01-14 19:31:28 +0530

    added upadtes like share button and submit on enter

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { FaShare } from "react-icons/fa6";
 import './style.css'
 
 export const Todo = () => {
@@ -18,16 +19,50 @@ export const Todo = () => {
     setTodos((todos) => todos.filter((t) => t !== input));
 
   }
+  const sharee = (input) =>
+  {
+    if (navigator.share) {
+      // Browser supports native share api
+      navigator.share({
+        text: `${input} - One Of My ToDo Today. Make Yours👇`,
+        url: 'https://toodlee.netlify.app/',
+      }).then(() => {
+      })
+        .catch((err) => console.error(err));
+    } else {
+      // Fallback
+      alert("The current browser does not support the share function. Please, manually share the link")
+    }
+  }
+  const shareeAll = () =>
+  {
+    if (navigator.share) {
+      // Browser supports native share api
+      navigator.share({
+        text: `${todos} - My ToDos Of Today. Make Yours👇`,
+        url: 'https://toodlee.netlify.app/',
+      }).then(() => {
+      })
+        .catch((err) => console.error(err));
+    } else {
+      // Fallback
+      alert("The current browser does not support the share function. Please, manually share the link")
+    }
+  }
+  
+
+  
   return (
     <>
     <div className='container'>
       <div className='card'>
-          <input className='input' type='text' value={input} onChange={(e) => setInput(e.target.value)} placeholder='Enter Your Activity'></input>
-                <button className='btn' onClick={handleSubmit}>Submit</button>
+          <input onKeyDown={(e) => {if (e.key === "Enter"){handleSubmit()}}} className='input' type='text' value={input} onChange={(e) => setInput(e.target.value)} placeholder='Enter Your Activity'></input>
+                <button type="submit" className='btn' onClick={handleSubmit}>Submit</button>
+                <button className='share' onClick={shareeAll}>Share <FaShare/></button>
                     <ul>
                       {todos.map((input) => (
-                        <li className='activity' key={Math.random()}><span>{input}</span>
-                          <button className='btn2' onClick={() => removeTodo(input)}>X</button>
+                        <li className='activity' key={Math.random()}><button className='btn2' onClick={() => sharee(input)}><FaShare /></button><span>{input}</span>
+                          <button  className='btn2' onClick={() => removeTodo(input)}>X</button>
                         </li>
                       ))}
                     </ul>
